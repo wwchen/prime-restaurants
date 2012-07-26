f6001b180390b1e5a45977d1af7e3c65a5f3524b
William Chen <williamchen16@gmail.com>
2012-07-25 23:59:24 -0500

    Separated restaurant's address into specific columns

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,9 @@
 class Restaurant < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :name, :street, :city, :state, :zip, :phone
+  validates :name,   :presence => true
+  validates :street, :presence => true
+  validates :city,   :presence => true
+  validates :state,  :presence => true
+  validates :zip,    :presence => true
+  validates :phone,  :length => { :minimum => 10 }
 end
--- db/migrate/20120726000553_create_restaurants.rb
+++ db/migrate/20120726000553_create_restaurants.rb
@@ -1,6 +1,9 @@
 class CreateRestaurants < ActiveRecord::Migration
   def change
     create_table :restaurants do |t|
+      t.string :name
+      t.string :address
+      t.text :telephone
 
       t.timestamps
     end
