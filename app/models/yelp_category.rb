3a2f9c7e7b967801d0cc334adbc65e79a0da8dfc
William Chen <williamchen16@gmail.com>
2012-11-11 12:38:09 -0600

    Changes to Yelp categories

--- app/models/yelp_category.rb
+++ app/models/yelp_category.rb
@@ -1,4 +1,5 @@
 class YelpCategory < ActiveRecord::Base
   has_and_belongs_to_many :yelp_infos
-  # attr_accessible :title, :body
+  attr_accessible :name
+  validates :name, :uniqueness => true
 end
