014e346d96b6339b346688e720c72109f1bdc74a
William Chen <williamchen16@gmail.com>
2012-12-02 15:20:18 -0800

    Adding flags and arguments to be more robust

--- script/crawl_microsoftprime.rb
+++ script/crawl_microsoftprime.rb
@@ -1,23 +1,53 @@
 #!/usr/bin/env ruby
 
 require 'mechanize'
-
-out_filename = ARGV.count < 1? 'restaurants.html' : ARGV[0]
-region = ARGV.count < 2? '-1' : ARGV[1]
-
-if ARGV.count < 2
-  puts "Usage: ./crawl_microsoftprime.rb <output-filename> <region-number>"
-  puts "No filename specified, using default filename, " + out_filename if ARGV.count < 1
-  puts "No region number specified, using default id, " + region        if ARGV.count < 2
-  puts "\n"
-end
+require 'optparse'
 
 agent = Mechanize.new
 agent.follow_meta_refresh = true
+region = -1
+out_filename = ARGV.count < 1? 'restaurants.html' : ARGV[0]
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: ./crawl_microsoftprime.rb <output-filename> <region-number>"
+
+  opts.on("-l", "--list-regions", "List all available regions") do |v|
+    page = agent.get 'http://microsoftprime.com'
+    regions = page.form.field_with(:name => 'ddlRegion')
+    regions.options.each do |region|
+      puts "#{region}\t#{region.text}"
+    end
+    exit
+  end
+  opts.on('-r', '--region', 'Region value to crawl') do |v|
+    region = v
+  end
+  opts.on_tail("-h", "--help", "Show help") do
+    puts opts
+    exit
+  end
+  opts.on_tail("-V", "--version", "Show version") do
+    puts OptionParser::Version.join('.')
+    exit
+  end
+end.parse!
+
+if ARGV.count < 1
+  puts "No filename specified, using default filename, " + out_filename if ARGV.count < 1
+end
 
 page = agent.get 'http://microsoftprime.com/'
-# wa_option = page.form.field_with(:name => 'ddlRegion').option_with(:text => 'Washington - Western')
-page.form.field_with(:name => 'ddlRegion').value = region # wa_option # washington - western
+
+region_selectlist = page.form.field_with(:name => 'ddlRegion')
+region_option = region_selectlist.option_with(:value => region)
+
+if region_option.nil?
+  puts "Invalid region value"
+  exit
+end
+
+puts "Selecting region #{region_option.text}"
+region_selectlist.value = region
 all_page = page.form.click_button(page.form.button_with(:name => 'cmdRegionGo'))
 food_page = all_page.link_with(:text => %r/food/i).click
 restaurant_page = food_page.link_with(:text => %r/restaurant/i).click
