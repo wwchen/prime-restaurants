a7c92233a294410a1411bad9678cd84712389da0
William Chen <williamchen16@gmail.com>
2012-12-17 16:54:26 -0600

    rails destroy scaffold YelpInfo

--- test/unit/helpers/yelp_infos_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class YelpInfosHelperTest < ActionView::TestCase
-end
--- test/unit/yelp_info_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class YelpInfoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
