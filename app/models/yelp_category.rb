33ccf086b663216e06e1200f50dad20a0d7b8aee
William Chen <williamchen16@gmail.com>
2012-12-17 16:53:55 -0600

    rails destroy scaffold YelpCategory

--- app/models/yelp_category.rb
+++
@@ -1,5 +0,0 @@
-class YelpCategory < ActiveRecord::Base
-  has_and_belongs_to_many :yelp_infos
-  attr_accessible :name
-  validates :name, :uniqueness => true
-end
--- test/unit/yelp_category_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class YelpCategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
