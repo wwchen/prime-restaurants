02da97c9e3a91767902439b1591f9af3537eac0a
William Chen <williamchen16@gmail.com>
2013-11-23 11:51:53 -0800

    Bootstrapping angular with yo
    Moving original code to site/

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('primeRestaurantsApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
