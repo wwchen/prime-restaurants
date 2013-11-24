47a96b05444759cbeda2f52859c92a2cb1d5c02b
William Chen <williamchen16@gmail.com>
2013-11-23 17:15:44 -0800

    Well, that was easy. Map markers get filtered out too

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,10 +3,18 @@
 angular.module('primeRestaurantsApp')
   .controller('MainCtrl', function ($scope, $http) {
     angular.extend($scope, {
-      seattle: { latitude: 47.626117, longitude: -122.332817 }
+      seattle: { latitude: 47.626117, longitude: -122.332817 },
+      map: {
+        center: $scope.seattle,
+        zoom: 10,
+        events: {
+        }
+      },
     });
 
     $http.get('data/restaurants.json').success(function (restaurants) {
       $scope.restaurants = restaurants;
+
+      console.log($scope);
     });
   });
