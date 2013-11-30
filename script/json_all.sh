ee73cd96bdb7fcd73cbdd75eec9fb98672f39a8b
William Chen <williamchen16@gmail.com>
2013-11-29 21:11:37 -0800

    Folder cleanup

--- script/json_all.sh
+++ script/json_all.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-file=`mktemp XXXXX`
-
-ruby crawl_microsoftprime.rb $file.html
-ruby parse_primelist.rb $file.html $file.json
-ruby parse_primelist.rb $file.html $file.txt
-mv $file.json all.json
-mv $file.txt all.txt
-rm $file $file.*
+rm -f prime.html
+echo "Craling prime website"
+ruby ./1_crawl_microsoftprime.rb -r 11 prime.html
+echo "Parsing html and converting to json"
+ruby ./2_parse_primelist.rb prime.html -t json
+echo "Post processing json"
+ruby ./3_process_json.rb prime.json -o prime-geocoded.json
