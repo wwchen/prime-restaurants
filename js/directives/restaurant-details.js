34cee3aa6199e7fd53d56616d2443dbfd8f23b20
William Chen <williamchen16@gmail.com>
2013-12-07 18:17:53 -0800

    I can haz dollar signs and photo carousels for restaurant details!

--- js/directives/restaurant-details.js
+++ js/directives/restaurant-details.js
@@ -3,17 +3,42 @@
 
 "use strict";
 
-angular.module('restaurantDetails', [])
+angular.module('restaurantDetails', ['dollarFilter', 'placePhotosDirective'])
 .directive('restaurantDetails', function () {
   return {
     restrict: 'AC', // attribute or class
     replace: false,
     templateUrl: 'partials/detail.html',
     scope: {
-      restaurant: '=model'
+      restaurant: '=model',
     },
     link: function ($scope) {
       $scope.Math = window.Math;
+      $scope.mapObject = -1;
+
+      $scope.$watch('mapObject', function(map) {
+        getPlaceDetails();
+      });
+
+      $scope.$watch('restaurant', function(res) {
+        getPlaceDetails();
+      });
+
+      var getPlaceDetails = function() {
+        var map = $scope.mapObject;
+        var res = $scope.restaurant;
+        if(map !== -1 && res && res.gplaces_ref) {
+          var service = new google.maps.places.PlacesService(map);
+          var request = { reference: res.gplaces_ref };
+          service.getDetails(request, function(place, status) {
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+              // the meat of everything
+              $scope.details = place;
+            }
+          });
+
+        }
+      };
     }
   }
 });
